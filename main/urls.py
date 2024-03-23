385df75b889f67819145838dbe0b7e5c1c293c19
Deineko Artem <68924024+TheDeinekoArtem@users.noreply.github.com>
2024-03-23 23:45:29 +0200

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', views.index),
+    path('about', views.about),
+    path('profile', views.profile_view, name="profile"),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('accounts/', include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
